6a1509e9237345f18ef39e199adfae978d480541
gauri <gauritondre@gmail.com>
2025-08-30 17:16:21 +0530

    services addes

---
+++ Desktop/block-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blockchainpage from './components/Blockchainpage';
+import AiPage from './components/AiPage';
+import SaasSection from './components/SaasSection';
+
+function App() {
+  
+
+  return (
+    <>
+    <SaasSection/>
+      {/* <Blockchainpage/> */}
+      {/* <AiPage/> */}
+    </>
+  )
+}
+
+export default App
